2ea233df2510822833a53859c2a23cfdbd75c770
70938 <70938@DESKTOP-TGU3M95>
2021-11-01 12:16:32 +0800

    javafx

---
+++ src/main/java/jzy_3d_sample/ui/AnglePanelController.java
@@ -0,0 +1,15 @@
+package jzy_3d_sample.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class AnglePanelController {
+
+    @FXML
+    private ListView<?> anglelist;
+
+    public ListView<?> getAnglelist() {
+        return anglelist;
+    }
+
+}
